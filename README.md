3cfb57c662ebc6444705733e05ad06258a591d9e
99btyler <99tylerberinger@gmail.com>
2022-12-02 18:59:27 -0500

    Added functionality

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # image-derivative-creator
-Resize an image to multiple sizes
+
+# Getting Started
+Get this project set up locally
+### Prerequisites
+* Pillow (PIL fork)
+  * `python3 -m pip install --upgrade pip`
+  * `python3 -m pip install --upgrade Pillow`
+### Setting up
+* Clone this repository
+* Using a text editor, open this repository
+### Running
+* Using terminal, navigate to src and type `python3 main.py`
---
+++ src/main.py
@@ -0,0 +1,33 @@
+import os
+from PIL import Image
+
+
+PATH_ORIGINALS = "originals"
+PATH_DERIVATIVES = "derivatives"
+
+
+for file in os.listdir(PATH_ORIGINALS):
+	
+	try:
+		image = Image.open(f"{PATH_ORIGINALS}/{file}")
+	except Exception as e:
+		print(f"Skipping file ({e})")
+		continue
+
+	print(f"====>{file}({image.width}x{image.height})")
+	widths_input = list(map(lambda width: width.strip(), input("List new widths by comma: ").split(",")))
+	for width in widths_input:
+		try:
+
+			file_path = os.path.splitext(file)
+			new_image_name = f"{file_path[0]}_{width}{file_path[1]}"
+			new_image_width = int(width)
+			new_image_height = int((new_image_width/image.width) * image.height)
+
+			new_image = image.resize((new_image_width, new_image_height))
+			new_image.save(f"{PATH_DERIVATIVES}/{new_image_name}")
+			print(f"Created {PATH_DERIVATIVES}/{new_image_name}")
+
+		except Exception as e:
+			print(f"Skipping width ({e})")
+			continue
